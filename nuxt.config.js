e5dd6b7e5a6131f11418e13318bea8ec1df2d404
Pooya Parsa <pooya@pi0.ir>
2017-06-05 02:17:46 +0430

    ⚡ use pwa module

--- nuxt.config.js
+++ nuxt.config.js
@@ -2,22 +2,21 @@ module.exports = {
   build: {
     extractCSS: true,
     extend(config, {isClient}) {
-      config.resolve.alias['create-api'] =
-        `./create-api-${isClient ? 'client' : 'server'}.js`
+      config.resolve.alias['create-api'] = `./create-api-${isClient ? 'client' : 'server'}.js`
     }
   },
   head: {
-    titleTemplate: 'Nuxt HN | %s',
+    titleTemplate: 'Nuxt HN | %s'
   },
-  loading: {color: '#ff6600'},
+  loading: {
+    color: '#ff6600'
+  },
   manifest: {
     theme_color: '#41B883'
   },
   modules: [
-    require('@nuxtjs/manifest'),
-    require('@nuxtjs/meta'),
-    require('@nuxtjs/workbox'),
-    require('@nuxtjs/component-cache')
+    '@nuxtjs/pwa',
+    '@nuxtjs/component-cache'
   ],
   plugins: [
     '~plugins/filters.js'
