11cc2582f9468fcacb87861ded46c1b2841adde4
Sebastien Chopin <sebastien.chopin@neo9.fr>
2017-06-05 18:07:38 +0200

    format file

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,22 +1,22 @@
 module.exports = {
   build: {
     extractCSS: true,
-    extend (config, {isClient}) {
+    extend(config, { isClient }) {
       config.resolve.alias['create-api'] = `./create-api-${isClient ? 'client' : 'server'}.js`
     }
   },
   head: {
     titleTemplate: 'Nuxt HN | %s',
     meta: [
-      {property: 'og:type', content: 'website'},
-      {property: 'og:title', content: 'Nuxt.js HackerNews'},
-      {property: 'og:description', content: 'HackerNews clone built with Nuxt.js'},
+      { property: 'og:type', content: 'website' },
+      { property: 'og:title', content: 'Nuxt.js HackerNews' },
+      { property: 'og:description', content: 'HackerNews clone built with Nuxt.js' },
       {
         property: 'og:image',
         content: 'https://cloud.githubusercontent.com/assets/904724/26784102/0d2f8000-49fc-11e7-8091-2b66901c73ee.png'
       },
-      {property: 'twitter:card', content: 'summary_large_image'},
-      {property: 'twitter:site', content: '@nuxt_js'},
+      { property: 'twitter:card', content: 'summary_large_image' },
+      { property: 'twitter:site', content: '@nuxt_js' },
     ]
   },
   loading: {
