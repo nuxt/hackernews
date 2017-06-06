676ead7f881eff32227f5b25b315d583118b5f8b
Pooya Parsa <pooya@pi0.ir>
2017-06-07 03:00:58 +0430

    update nuxt.config.js for pwa preset

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,16 +1,13 @@
 module.exports = {
   build: {
     extractCSS: true,
-    extend(config, { isClient }) {
+    extend (config, { isClient }) {
       config.resolve.alias['create-api'] = `./create-api-${isClient ? 'client' : 'server'}.js`
     }
   },
   head: {
     titleTemplate: 'Nuxt HN | %s',
     meta: [
-      { property: 'og:type', content: 'website' },
-      { property: 'og:title', content: 'Nuxt.js HackerNews' },
-      { property: 'og:description', content: 'HackerNews clone built with Nuxt.js' },
       {
         property: 'og:image',
         content: 'https://cloud.githubusercontent.com/assets/904724/26784102/0d2f8000-49fc-11e7-8091-2b66901c73ee.png'
@@ -25,24 +22,7 @@ module.exports = {
   manifest: {
     name: 'Nuxt Hacker News',
     description: 'HackerNews clone built with Nuxt.js',
-    theme_color: '#41b883',
-    icons: [
-      {
-        src: '/favicon.ico',
-        sizes: '16x16',
-        type: 'image/x-icon'
-      },
-      {
-        src: '/logo_192.png',
-        sizes: '192x192',
-        type: 'image/png'
-      },
-      {
-        src: '/logo_512.png',
-        sizes: '512x512',
-        type: 'image/png'
-      }
-    ]
+    theme_color: '#41b883'
   },
   modules: [
     '@nuxtjs/pwa',
@@ -55,8 +35,8 @@ module.exports = {
   render: {
     static: {
       maxAge: '1y',
-      setHeaders: function (res, path) {
-        if (path.includes('sw.js') || path.includes('workbox-sw.')) {
+      setHeaders (res, path) {
+        if (path.includes('sw.js')) {
           res.setHeader('Cache-Control', 'public, max-age=0')
         }
       }
