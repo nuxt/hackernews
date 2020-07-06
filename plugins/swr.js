c8bd5fc8b59784ecdbb8c377075422ff22bcec0b
Daniel Roe <daniel@concision.co.uk>
2020-07-06 15:53:48 +0100

    fix: move swr hook to mounted and add query param to override
    avoids node mismatch

--- plugins/swr.js
+++ plugins/swr.js
@@ -3,15 +3,19 @@ import Vue from 'vue'
 const currentTime = new Date().getTime()
 
 Vue.mixin({
-  beforeMount () {
-    if ('$fetch' in this && currentTime - this.$store.state.swr.time > 30000) {
+  mounted () {
+    if (
+      '$fetch' in this &&
+      (currentTime - this.$store.state.swr.time > 30000 ||
+        Object.keys(this.$route.query).includes('refresh'))
+    ) {
       this.$fetch()
     }
   }
 })
 
 export default function ({ store }) {
-  store.registerModule('swr',{
+  store.registerModule('swr', {
     state: () => ({
       time: currentTime
     })
