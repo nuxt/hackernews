4b860a7a2eebb263f85da638d1ac963ead3d277b
Anthony Fu <anthonyfu117@hotmail.com>
2022-04-19 21:40:44 +0800

    fix: avoid using `abortNavigation` for now

--- middleware/feed.ts
+++ middleware/feed.ts
@@ -1,6 +1,6 @@
 export default defineNuxtRouteMiddleware((from) => {
   if (!from.params.feed || !validFeeds.includes(from.params.feed as string)) {
-    return abortNavigation('not found')
+    return navigateTo('/')
   }
   if (!from.params.page) {
     return navigateTo(`/${from.params.feed}/1`)
