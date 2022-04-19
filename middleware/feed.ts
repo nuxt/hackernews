ecca97d4496b9650bad8628056f4f6c04c0ad0e7
Anthony Fu <anthonyfu117@hotmail.com>
2022-04-19 12:41:33 +0800

    chore: update ignore

--- middleware/feed.ts
+++ middleware/feed.ts
@@ -1,6 +1,8 @@
 export default defineNuxtRouteMiddleware((from) => {
-  if (!from.params.feed || !validFeeds.includes(from.params.feed as string))
-    return abortNavigation()
-  if (!from.params.page)
+  if (!from.params.feed || !validFeeds.includes(from.params.feed as string)) {
+    return abortNavigation('not found')
+  }
+  if (!from.params.page) {
     return navigateTo(`/${from.params.feed}/1`)
+  }
 })
