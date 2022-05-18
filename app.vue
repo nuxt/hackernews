b586fb94f6cbb98be4b6c440271b96409a5b4708
Anthony Fu <anthonyfu117@hotmail.com>
2022-05-18 19:32:25 +0800

    chore: update ogimage

--- app.vue
+++ app.vue
@@ -2,7 +2,7 @@
 useHead({
   titleTemplate: 'Nuxt HN | %s',
   meta: [
-    { property: 'og:image', content: 'https://user-images.githubusercontent.com/904724/58238637-f189ca00-7d47-11e9-8213-ae072d7cd3aa.png' },
+    { property: 'og:image', content: 'https://user-images.githubusercontent.com/11247099/169022756-cdb6ef6f-1299-4ce0-8f80-81fb6e86a2e1.png' },
     { property: 'twitter:card', content: 'summary_large_image' },
     { property: 'twitter:site', content: '@nuxt_js' }
   ],
