863340b069077885280d54cc304803672da6adbe
Sébastien Chopin <seb@nuxt.com>
2024-06-17 18:42:36 +0200

    chore: update

--- app/app.vue
+++ app/app.vue
@@ -2,17 +2,12 @@
 useSeoMeta({
   titleTemplate: 'Nuxt HN | %s',
   description: 'HackerNews clone built with Nuxt.',
-  ogImage: 'https://hn.nuxt.space/cover.jpg',
+  ogImage: 'https://hn.nuxt.dev/cover.jpg',
   ogImageAlt: 'Nuxt Hackernews',
-  twitterImage: 'https://hn.nuxt.space/cover.jpg',
+  twitterImage: 'https://hn.nuxt.dev/cover.jpg',
   twitterSite: '@nuxt_js',
   twitterCreator: '@nuxt_js',
   twitterCard: 'summary_large_image',
-})
-useHead({
-  link: [
-    { rel: 'icon', type: 'image/svg+xml', href: '/logo.svg' },
-  ],
 })
 </script>
 
